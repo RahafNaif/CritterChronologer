1ef1d7cac21408346a6db7610c9aef6846870a57
RahafNaif <49600567+RahafNaif@users.noreply.github.com>
2023-08-25 18:29:26 +0300

    init

---
+++ critter/src/main/java/com/udacity/jdnd/course3/critter/user/dto/CustomerDTO.java
@@ -0,0 +1,13 @@
+package com.udacity.jdnd.course3.critter.user.dto;
+
+import lombok.Data;
+import java.util.List;
+
+@Data
+public class CustomerDTO {
+    private long id;
+    private String name;
+    private String phoneNumber;
+    private String notes;
+    private List<Long> petIds;
+}
---
+++ critter/src/main/java/com/udacity/jdnd/course3/critter/user/dto/EmployeeDTO.java
@@ -0,0 +1,14 @@
+package com.udacity.jdnd.course3.critter.user.dto;
+
+import com.udacity.jdnd.course3.critter.user.EmployeeSkill;
+import lombok.Data;
+import java.time.DayOfWeek;
+import java.util.Set;
+
+@Data
+public class EmployeeDTO {
+    private long id;
+    private String name;
+    private Set<EmployeeSkill> skills;
+    private Set<DayOfWeek> daysAvailable;
+}
